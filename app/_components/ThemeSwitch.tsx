c9525f45cdbd49acff6e6a440c95c9278c732fc0
DAQEM <daqemyt@gmail.com>
2024-05-13 16:45:43 +0200

    Added dark theme to switch

--- app/_components/ThemeSwitch.tsx
+++ app/_components/ThemeSwitch.tsx
@@ -19,6 +19,7 @@ const ThemeSwitch = () => {
             </option>
             <option value="">Default</option>
             <option value="light">Light</option>
+            <option value="dark">Dark</option>
             <option value="retro">Retro</option>
             <option value="dracula">Dracula</option>
             <option value="valentine">Valentine</option>
