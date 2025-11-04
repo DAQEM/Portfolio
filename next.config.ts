886dcf6d7b5a53fe57ec3da1e96ee3c29d40a48c
Kevin <daqemyt@gmail.com>
2025-11-04 09:48:13 +0100

    fix: made images unoptimized

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import createMDX from "@next/mdx";
 const nextConfig: NextConfig = {
     pageExtensions: ["mdx", "ts", "tsx"],
     output: "export",
+    images: {
+        unoptimized: true,
+    },
 };
 
 const withMDX = createMDX({});
